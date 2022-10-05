9c9af8787c6751365c6aaf24c9306115d95ac32a
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-05 19:40:30 +0500

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -5,21 +5,15 @@ from create_dataset import create
 from tj import *
 from lemmanization import human_to_computer
 
+
 def parse(f_text, f_title, f_lead_text, f_tags, f_views, href):
-    with open("csv_file.csv", "w", encoding="cp1251") as file:
-        writer = csv.writer(file)
+    with open("csv_file.csv", "w", encoding="cp1251") as file:  # Эти костыли надо срочно переделывать.
+        writer = csv.writer(file) # Я хз как, но каждый раз бд перезаписывается заново
         create(("human_text", "machine_text", "title", "tags", "undername", "href", "views", "comments", "for_who"),
                writer)
-        try:
-            r = requests.get(href)
-        except:
-            return
 
-        html = BS(r.content, 'html.parser')
-        href_in_code = html.find_all("a", class_="link--pdufU")
-        for i in href_in_code:
-            str_link = str(i.get("href"))
-
+        str_links = tj_get_hrefs()
+        for str_link in str_links:
             r = requests.get(f'https://journal.tinkoff.ru{str_link}')
             html = BS(r.content, 'html.parser')
             print(str_link)
