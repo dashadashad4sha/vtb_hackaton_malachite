2b211f96af75d17247fd8f9c792ad4e34928d82a
Daria Matveeva <90855097+dashadashad4sha@users.noreply.github.com>
2022-10-09 05:02:49 +0500

    Update for_bot.py

--- for_bot.py
+++ for_bot.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from ml import prediction_for_acc, prediction_for_gd
-
+from no_doubles import doubles_find
 dataset_example = pd.read_csv(f"csv_file.csv", encoding='cp1251')
 
 
@@ -13,10 +13,8 @@ def super_main_def(role, dataset):
     if role == "Гендир":
         dataset["raiting"] = prediction_for_gd(df)
 
-    # Сейчас датафрейм с отмеченным рейтингом в переменной dataset
-
     sorted_ds = dataset.sort_values(by='raiting', ascending=False).head(10)
 
-    # Надо убрать дублирования и положить в переменную sorted_ds
+    sorted_ds = doubles_find(sorted_ds)
 
-    return sorted_ds["href"].head(3)
+    return sorted_ds["href"].head()
